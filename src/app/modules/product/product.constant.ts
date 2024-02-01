0cda3a083e9dde5e401c5ed5d78c2d879b269523
nakib1948 <hayathossain.jtp@gmail.com>
2024-02-01 23:13:36 +0600

    product database query complete

--- src/app/modules/product/product.constant.ts
+++ src/app/modules/product/product.constant.ts
@@ -1,4 +1,14 @@
-import { bookFormat } from "./product.interface";
+import { bookFormat } from './product.interface';
 
+export const bookformat: bookFormat[] = ['hardcover', 'paperback', 'e-book'];
 
-export const bookformat: bookFormat[] = ['hardcover','paperback','e-book']
+export const bookSearchableFields = [
+  'date',
+  'author',
+  'isbn',
+  'genre',
+  'publisher',
+  'series',
+  'language',
+  'format',
+];
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,12 +1,43 @@
-import { TBook } from "./product.interface";
-import { Product } from "./product.model";
+import QueryBuilder from '../../builder/Querybuilder';
+import { bookSearchableFields } from './product.constant';
+import { TBook } from './product.interface';
+import { Product } from './product.model';
 
+const createBook = async (book: TBook) => {
+  return await Product.create(book);
+};
 
-const createBook = async(book:TBook)=>{
-    return await Product.create(book)
-}
+const getAllBooks = async (query: Record<string, unknown>) => {
+  const bookQuery = new QueryBuilder(Product.find(), query)
+    .search(bookSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+  const result = await bookQuery.modelQuery;
+  const metaData = await bookQuery.countTotal();
+
+  return {
+    data: result,
+    meta: metaData,
+  };
+};
+
+const updateBookById = async (bookId: string, payload: Partial<TBook>) => {
+  const result = await Product.findByIdAndUpdate({ _id: bookId }, payload, {
+    new: true,
+    runValidators: true,
+  });
+  return result;
+};
+
+const deleteBookById = async (BookId: string[]) => {
+  const result = await Product.deleteMany({ id: { $in: BookId } });
+  return result;
+};
 
 export const productServices = {
-    createBook
-  };
-  
+  createBook,
+  getAllBooks,
+  deleteBookById
+};
